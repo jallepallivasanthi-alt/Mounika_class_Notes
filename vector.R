6e55e6bfb5e6705ec76abc8acf1b663f79a75d08
MounikaGaddam22 <reddymouna24@gmail.com>
2025-08-06 11:10:54 -0500

    Create vector.R
    VECTOR CLASSES

---
+++ vector.R
@@ -0,0 +1,71 @@
+#create a vector 
+#add a new value to the vector
+#Vector Operations
+
+#Create a variable
+#Create a Vector
+#add a new element to the vector
+#Access the vector
+#Access the two values from the same vector
+# Delete a vector
+#delete a value from the vector
+#Check the length of the vector
+# Replace a value in vector
+# add two vectors
+# create a dataframe using vectors
+# create a list using vectors
+# delete a value in vector
+# Create a vector using multiple data type
+#Check the type of the data structure
+#convert the vector into list
+# Create an another variable using a value from the vector
+#Type conversion
+
+
+#Create a variable
+example<-100
+example<-1000
+example
+vector_var<-100,200,700,800
+vector_var<-c(100,200,700,800)
+base::class(vector_var)
+base::class(example_2)
+example_2<-"hello"
+multi_vector<-c(100.1,"hello",TRUE)
+base::class(multi_vector)
+example_3<-500
+base::class(example_3)
+example*4
+vector_var
+example_4=1
+multi_vector*4
+length(vector_var)
+length(example)
+vector_var[4]
+result<-vector_var[4]
+result
+multi_vector[2]
+result_2<-multi_vector[2]
+result_2
+add_vector<-c(result,result_2)
+add_vector
+length(add_vector)
+add_vector[2]
+char_vector<-c("spark","python","R","java","jsp")
+length(char_vector)
+char_vector[4]<-"C"
+char_vector
+char_vector_2<-char_vector[3]<-10
+char_vector_2
+char_vector
+char_vector[c(1,5)]<-c(15,20)
+char_vector<-200
+char_vector
+char_vector[char_vector<15]<-100
+char_vector
+numeric_vector<-as.numeric(char_vector)
+numeric_vector
+is.vector(numeric_vector)
+numeric_vector["value"]<-200
+numeric_vector[-2]
+length(numeric_vector)
