625a0c523c78538fda53f52de50a15c23481f375
jallepallivasanthi-alt <jallepallivasanthi@gmail.com>
2025-08-12 21:24:41 +0530

    Create data_manipulation.R

---
+++ data_manipulation.R
@@ -0,0 +1,20 @@
+library(dplyr)
+# Create DataFrame
+df <- data.frame(
+  id = c(10,11,12,13,14,15,16,17),
+  name = c('sai','ram','deepika','sahithi','kumar','scott','Don','Lin'),
+  gender = c('M','M','F','F','M','M','M','F'),
+  dob = as.Date(c('1990-10-02','1981-3-24','1987-6-14','1985-8-16',
+                  '1995-03-02','1991-6-21','1986-3-24','1990-8-26')),
+  state = c('CA','NY',NA,NA,'DC','DW','AZ','PH'),
+  row.names=c('r1','r2','r3','r4','r5','r6','r7','r8')
+)
+df
+gender_M<-dplyr::filter(df,gender=="M")
+dob_df<-select(df,dob,state)
+gender_M
+dob_df
+#create a new column based on condition
+piping_df<-df%>%mutate(branch=c("cse","it","ece","cse","it","eee",NA,NA))%>%
+ select(c(name,state,branch))%>%
+  filter(branch=="cse")
